876ea1d6b45f1498595cad74bffa7dfd8b822c01
liambilahezron <liambilahezron1990@gmail.com>
2022-08-23 16:14:04 +0300

    correction

--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,33 +1,51 @@
 #include "lists.h"
-
 /**
- * insert_node - Inserts a number into a sorted singly-linked list.
- * @head: A pointer the head of the linked list.
- * @number: The number to insert.
- * Return: If the function fails - NULL.
+ * insert_node - inserts a node in a linked list
+ * @head: double pointer to the head of the list
+ * @number: value to be added to node
+ * Return: the address of the new node, or NULL if it failed
  */
 
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *node = *head, *new;
+	listint_t *current, *temp, *new;
+
+	current = *head, temp = *head, new = NULL;
 
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
-	new->n = number;
-
-	if (node == NULL || node->n >= number)
+	if (*head == NULL)
 	{
-		new->next = node;
+		new->n = number;
+		new->next = NULL;
 		*head = new;
 		return (new);
 	}
-
-	while (node && node->next && node->next->n < number)
-		node = node->next;
-
-	new->next = node->next;
-	node->next = new;
-
+	if (temp == NULL)
+		return (NULL);
+	if (temp->n >= number)
+	{
+		new->n = number;
+		new->next = temp;
+		*head = new;
+		return (new);
+	}
+	while (current->next)
+	{
+		current = current->next;
+		if (current->n >= number)
+		{
+			new->n = number;
+			new->next = current;
+			temp->next = new;
+			return (new);
+		}
+		else
+			temp = temp->next;
+	}
+	new = add_nodeint_end(head, number);
 	return (new);
 }
+Footer
+
