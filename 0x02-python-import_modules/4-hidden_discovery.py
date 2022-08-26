906436f27ea1a79bd6e4f1ff6702b3a09f31a7f0
liambilahezron <liambilahezron1990@gmail.com>
2022-08-26 06:55:03 +0300

    made file excutable

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 import hidden_4
 
-if __name__ == '__main__':
-    def_names = dir(hidden_4)
 
-    for i in range(len(def_names)):
-        if def_names[i][:2] != '__':
-            print(def_names[i])
+def discovr():
+    name = dir(hidden_4)
+    for i in name:
+        if i[:2] != '__':
+            print("{:s}".format(i))
+
+
+if __name__ == "__main__":
+    discovr()
