6ac8a1927bb149c764ad036896189a74156d844e
liambilahezron <liambilahezron1990@gmail.com>
2022-09-12 16:56:40 +0300

    python-exceptions done

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
-def weight_average(my_list=[]):
-    if my_list == [] or my_list is None:
-        return (0)
-    res = 0
-    res2 = 0
-    for x, y in my_list:
-        res += x * y
-        res2 += y
-    return (res / res2)
+def mutiply_list_map(my_list=[], number=0):
+    return (list(map(lambda x: x*number, my_list)))
