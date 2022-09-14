4ff028a53a9cacbe959714f01b2ad0d185a38ea2
liambilahezron <liambilahezron1990@gmail.com>
2022-09-14 12:13:18 +0300

    python-classes

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -3,17 +3,20 @@
 
 
 class Square:
-    """ Defines the Square class"""
+    """ Define the Square Class    """
     def __init__(self, size=0, position=(0, 0)):
-        self.__size = size
+        """initialize """
+        self.size = size
         self.position = position
 
     @property
     def size(self):
+        """ Getter of size """
         return self.__size
 
     @size.setter
     def size(self, value):
+        """ Setter of size """
         if type(value) != int:
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -23,11 +26,12 @@ class Square:
 
     @property
     def position(self):
+        """ Get postion attribute """
         return self.__position
 
     @position.setter
     def position(self, value):
-
+        """ Setter of position """
         if len(value) != 2 or type(value) != tuple:
             raise TypeError("position must be a tuple of 2 positive integers")
         if type(value[0]) != int or value[0] < 0:
@@ -37,14 +41,24 @@ class Square:
         self.__position = value
 
     def area(self):
-        return self.__size * self.__size
+        """ Get area """
+        return self.__size ** 2
 
     def my_print(self):
-        if self.size != 0:
-            if self.position[1] is not 0:
-                print('\n' * self.position[1], end='')
-            for i in range(self.size):
-                print(' ' * self.position[0], end='')
-                print('#' * self.size)
-        else:
+        """ Print a square """
+        if self.size == 0:
             print()
+        else:
+            print('\n'*self.__position[1], end='')
+            for i in range(self.__size):
+                print(' '*self.__position[0], end='')
+                print('#'*self.__size)
+
+    def __str__(self):
+        """ Defining printing behavior of the class """
+        if self.__size == 0:
+            return ''
+        new_lines = '\n' * self.position[1]
+        spaces = ' ' * self.position[0]
+        hashes = '#' * self.size
+        return new_lines + '\n'.join(spaces + hashes for e in range(self.size))
